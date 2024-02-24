400b8df138631de30fa52c7d14860b99d41373f6
Todd Butters <git@todd.btrz.org>
2024-02-24 10:16:45 -0500

    keyd: update syntax for 23.11 breaking change
    Updated api parameters allows different settings for different keyboards.
    https://github.com/NixOS/nixpkgs/pull/243271#issuecomment-1634247494

--- keyd.nix
+++ keyd.nix
@@ -1,18 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  services.keyd.enable = true;
-  services.keyd.settings = 
-  {
-	main = {
-	  capslock = "overload(control, esc)";
-	  rightalt = "layer(rightalt)";
-	};
-	rightalt = {
-	  h = "left";
-	  j = "down";
-	  k = "up";
-	  l = "right";
-	};
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        settings = {
+          main = {
+            capslock = "overload(control, esc)";
+            rightalt = "layer(rightalt)";
+          };
+          rightalt = {
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+          };
+        };
+      };
+    };
   };
 }
