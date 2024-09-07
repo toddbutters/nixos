34206bc03f3f1b7890752b7920bf0162d5e031c2
Todd Butters <git@todd.btrz.org>
2024-09-07 12:50:53 -0400

    atuin: use version from unstable channel

--- atuin.nix
+++ atuin.nix
@@ -1,8 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  unstable = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixos-unstable)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in
+
 {
   # Install atuin package to system and add to path.
-  environment.systemPackages = with pkgs; [ atuin ];
+  environment.systemPackages = with pkgs; [ unstable.atuin ];
 
   services.atuin = {
     enable = true;
@@ -10,6 +17,7 @@
 
   programs.bash = {
     interactiveShellInit = ''
+      shopt -s histappend
       eval "$(atuin init bash)"
     '';
   };
