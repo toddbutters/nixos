da5a61ad81328a1eac1552f4b6825f1a5a619bb8
Todd Butters <git@todd.btrz.org>
2024-09-07 13:03:00 -0400

    gnome: basic config with pop-shell and pano
    - basic gnome desktop
    - pop-shell window tiling extension
    - pano clipboard manager extension

---
+++ gnome.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+{
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.pop-shell
+    gnomeExtensions.pano
+  ];
+}
